6719a2a71da5d4d9c5829b25e6128385c0b663c4
Michael Mattini <michaelmattini@Michaels-MacBook-Pro.local>
2018-05-26 09:53:37 -0500

    Second

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,7 +1,24 @@
 @extends('layout.app')
 
 @section('content')
-<h1>Welcome to Serenity</h1>
-<h2>About the crew</h2>
-<h3>Noone can take the sky from me</h3>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+
+            <h1>Meet The Fat Bearded Vegan!</h1>
+
+        </div>
+    </div>
+</div>
+
+
+<div class="media">
+        <div class="media-left">
+          <img src="img_avatar1.png" class="media-object" style="width:60px">
+        </div>
+        <div class="media-body">
+          <h4 class="media-heading">John Doe</h4>
+          <p>Lorem ipsum...</p>
+        </div>
+      </div>
 @endsection
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,8 +1,16 @@
 @extends('layout.app')
 
 @section('content')
-<div class="jumbotron jumbotron-fluid">
-    <h1 class="display-4">Welcome to Serenity</h1>
-    <p class="lead">Noone can take the sky from me</p>
-</div>
+    <div class="jumbotron">
+            <h1 class="display-4">Welcome to Serenity</h1>
+            <p class="lead">You can't take the sky from me</p>
+    </div>
+    <div class="row">
+        <div class="col-md-8">
+            <p class="lead">This is where my text should go</p>
+        </div>
+        <div class="col-md-4">
+            <h3>Sidebar</h3>
+    </div>
+
 @endsection
