ae7cafc72ac0c075a5e2cb4ac3d59f1d05dafbe2
Michael Mattini <michaelmattini@Michaels-MBP.attlocal.net>
2018-07-29 23:35:50 -0500

    Fixed errors

--- resources/views/emails/email.blade.php
+++ resources/views/emails/email.blade.php
@@ -4,13 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js" integrity="sha384-u/bQvRA/1bobcXlcEYpsEdFVK/vJs3+T+nXLsBYJthmdBuavHvAW6UsmqO2Gd/F9" crossorigin="anonymous"></script>
+        <link href="{{ asset('css/boot.css') }}" rel="stylesheet">
     <title>Email from TheFatBeardedVegan.com</title>
 </head>
 <body>
   <div class="container">
       <div class="row bg-dark shadow-sm">
           <div class="col-3">
-              <img src="images/veganlogo.jpg">
+              <img src="http://serenity.dev/images/veganlogo.jpg" class="img-thiumbnail" style="width:100px;height:100px;" alt="Vegan Logo">
           </div>
           <div class="col-9">
               <h2>THE FAT BEARDED VEGAN</h2>
--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -1,9 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title></title>
-</head>
-<body>
+
 	<nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-lg">
 		<div class="container">
 			<img src="/images/veganlogo.png" class="img-thiumbnail" style="width:100px;height:100px;" alt="Vegan Logo">&nbsp;&nbsp;
@@ -26,9 +21,32 @@
 						<a class="nav-link" href="/contact">Contact</a>
 					</li>
 				</ul>
+				<div class="collapse navbar-collapse" id="navbarSupportedContent">
+					<ul class="navbar-nav ml-auto">
+						<!-- Authentication Links -->
+						<li style="list-style: none">@guest</li>
+						<li>
+							<a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+					</li>
+						<li>
+							<a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+						</li>
+						<li style="list-style: none">@else</li>
+						<li class="nav-item dropdown">
+							<a aria-expanded="false" aria-haspopup="true" class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" id="navbarDropdown" role="button">{{ Auth::user()->name }} <span class="caret"></span></a>
+							<div aria-labelledby="navbarDropdown" class="dropdown-menu">
+               <a class="dropdown-item" href="/home">Dashboard</a>
+               <a class="dropdown-item" href="/posts/create">Create Post</a>
+								<a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+								<form action="{{ route('logout') }}" id="logout-form" method="post" name="logout-form" style="display: none;">
+									@csrf
+								</form>
+							</div>
+						</li>
+						<li style="list-style: none">@endguest</li>
+					</ul>
+				</div>
 				
 			</div>
 		</div>
 	</nav>
-</body>
-</html>
